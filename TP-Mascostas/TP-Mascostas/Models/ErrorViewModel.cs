eaa03e4f95df05d10be1a7e99ebfb62736a48b49
Santiago Linietsky <santiagolky@gmail.com>
2019-05-29 23:46:30 -0300

    Start

---
+++ TP-Mascostas/TP-Mascostas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TP_Mascostas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
