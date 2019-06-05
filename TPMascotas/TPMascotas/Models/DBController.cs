49951e598ff38e8404ad8ad521e0ab96ab27916a
Santiago Linietsky <santiagolky@gmail.com>
2019-06-05 18:58:11 -0300

    act

---
+++ TPMascotas/TPMascotas/Models/DBController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TPMascotas.Models
+{
+    public class DBController : Controller
+    {
+        // GET: DB
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
