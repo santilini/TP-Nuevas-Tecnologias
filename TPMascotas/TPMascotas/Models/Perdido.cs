7d276abbd2cd87c566a21aec27a6c31d1a7deb95
Santiago Linietsky <santiagolky@gmail.com>
2019-06-12 20:54:24 -0300

    Entity

---
+++ TPMascotas/TPMascotas/Models/Perdido.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TPMascotas.Models
+{
+    public class Perdido
+    {
+        
+        public String tipo { get; set; }
+        public String sexo { get; set; }
+        public String raza { get; set; }
+        public String desc { get; set; }
+
+    }
+}
